a8b9ae7a0ce15419fa80058eb2650f24634e503b
LXQ <515835543@qq.com>
2018-12-17 14:30:52 +0800

    fixed login

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,105 +1,15 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import './core.css';
-// import 'antd-mobile/dist/antd-mobile.css';
-// import All from './containers/all';
-import {createStore,combineReducers} from 'redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import Login from './containers/login/login'
+import * as serviceWorker from './serviceWorker';
 
-// state:保存在store种的数据
-// action：容器
-// type:简单的字符串常量
-// payload：用于更新状态的数据
+ReactDOM.render(
+    <Login />, document.getElementById('root')
+);
 
-
-
-// 创建reducer方法
-
-const initialState = {
-    cart:[
-        {
-            product:'bread 700g',
-            quantity:2,
-            unitCost:90
-        },
-        {
-            product:'milk 500ml',
-            quantity:1,
-            unitCost:47
-        }
-    ]
-}
-
-const productsReducer = function(state=[],action){
-    return state;
-}
-
-const cartReducer = function(state=initialState,action){
-    return state;
-}
-
-const allReducers = {
-    products:productsReducer,
-    cart:cartReducer
-}
-
-const rootReducer = combineReducers(allReducers);
-
-
-let store = createStore(rootReducer);
-
-console.log('initial state:',store.getState());
-
-const ADD_TO_CART = 'ADD_TO_CART';
-
-// const cartReducer = function(state=initialState,action){
-//     switch(action.type){
-//         case ADD_TO_CART:{
-//             return{
-//                 ...state,
-//                 cart:[...state.cart,action.payload]
-//             }
-//         }
-
-//         default:
-//             return state;
-//     }
-// }
-
-// function addToCart(product,quantity,unitCost){
-//     return{
-//         type:ADD_TO_CART,
-//         payload:{product,quantity,unitCost}
-//     }
-// }
-
-// let unsubscribe = store.subscribe(() =>
-//   console.log(store.getState())
-// );
-
-// unsubscribe();
-
-// ReactDOM.render(
-//     <All />,
-//     document.getElementById('root')
-// )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
